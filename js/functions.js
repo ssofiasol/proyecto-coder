3bc57ea596b36c0a7f3d3621630bb364b9d0c8bc
ssofiasol <rsofiasol@gmail.com>
2021-12-30 22:23:45 -0300

    JSONyLS

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,29 @@
 
-const carrito = new Carrito();
+let jsonCarrito;
+let carrito = new Carrito();
+
+function agregarDivItemCarrito(nombre, precio) {
+    let itemDelCarrito = document.createElement("div");
+        itemDelCarrito.classList.add("estilosCarrito");
+        let textItemDelCarrito = document.createTextNode(`${nombre} $${precio}`);
+        itemDelCarrito.appendChild(textItemDelCarrito);
+        const divCarrito = document.getElementById("carrito")
+        divCarrito.insertBefore(itemDelCarrito, divCarrito.firstChild);
+}
+
+if (localStorage.getItem('carrito')) {
+    jsonCarrito = JSON.parse(localStorage.getItem('carrito'))
+} else {
+    jsonCarrito = carritoInicial
+}
+
+for (let index = 0; index < jsonCarrito.items.length; index++) {
+    const element = jsonCarrito.items[index];
+    const producto = new Producto(element.producto.nombre, parseInt(element.producto.precio), 0)
+    carrito.agregarItem(new ItemCarrito(element.cantidad, producto))
+    agregarDivItemCarrito(producto.nombre, producto.precio)
+}
+document.getElementById("totalCompra").innerText = `Total: $${carrito.calcularTotal()}`
 
 let botonesDanger = document.getElementsByClassName("btn-danger");
 
@@ -11,13 +35,9 @@ for (let index = 0; index < botonesDanger.length; index++) {
         const producto = new Producto(nombreProducto, parseInt(precioProducto), 0)
         carrito.agregarItem(new ItemCarrito(1, producto))
         element.innerText = "Agregar más";
-        let itemDelCarrito = document.createElement("div");
-        itemDelCarrito.classList.add("estilosCarrito");
-        let textItemDelCarrito = document.createTextNode(`${producto.nombre} $${producto.precio}`);
-        itemDelCarrito.appendChild(textItemDelCarrito);
-        const divCarrito = document.getElementById("carrito")
-        divCarrito.insertBefore(itemDelCarrito, divCarrito.firstChild);
+        agregarDivItemCarrito(producto.nombre, producto.precio);
         document.getElementById("totalCompra").innerText = `Total: $${carrito.calcularTotal()}`
+        localStorage.setItem('carrito', JSON.stringify(carrito))
     }
 }
 
